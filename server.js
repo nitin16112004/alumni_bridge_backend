e8aad9c7b09e0671d924f1109c9f79de6e01424e
Nitin Kumar <155599140+nitin16112004@users.noreply.github.com>
2026-05-21 10:01:04 +0530

    Update import paths for database and socket

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ require('dotenv').config({ path: require('path').join(__dirname, '.env') });
 const http = require('http');
 const { Server } = require('socket.io');
 const app = require('./src/app');
-const connectDB = require('./src/config/db');
-const initSocket = require('./src/socket/index');
+const connectDB = require('./config/db');
+const initSocket = require('./socket/index');
 
 const PORT = process.env.PORT || 5000;
 
